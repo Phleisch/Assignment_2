7ced8ea838d8b5f7af606ce4a77024b004e77f5b
Kai Fleischman <kaifleischmans@gmail.com>
2020-11-09 21:56:39 +0100

    First attempt at ex_1

---
+++ ex_1/exercise_1.cu
@@ -0,0 +1,36 @@
+#include <stdio.h>	// For use of the printf function
+#define N 256		// Number of threads to use
+#define TPB 256		// Threads PER block
+
+/**
+ * Function launched from the CPU and run on the GPU that will display a message
+ * of the format `Hello World! My threadId is x` where x is the the threadId of
+ * the thread found by thread indexing.
+ */
+__global__ void hello_world_kernel() {
+
+	// Calculate a unique ID for the currently running thread by using its
+	// index within a block plus an offset of number of threads before the
+	// current block (blockIdx.x * blockDim.x)
+	int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	// Print a message of format 'Hello World! My threadId is x' where x is
+	// thread_id
+	printf("Hello World! My threadId is %d\n", thread_id);
+}
+
+// Entry point into the program, initiate kernel launch
+int main() {
+
+	// Run the hello_world_kernel function on device using N / TPB thread
+	// blocks of TPB threads PER block
+	hello_world_kernel <<<(N / TPB), TPB>>> ();
+
+	// CRUCIAL! Make execution of the kernel synchronous so that the CPU waits
+	// for all threads in the grid to complete before finishing the program.
+	// Without this you will not see the result of calls to printf in the kernel
+	// function due to the default asynchronous nature of kernel launches
+	cudaDeviceSynchronize();
+
+	return 0;
+}
